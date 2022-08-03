813a1eecefe82cd05c3b7030b1a591f2bde60643
EL Gabo <harcot.sys@gmail.com>
2022-08-02 21:28:07 -0500

    el carrito ya esta

--- yardSale/main.js
+++ yardSale/main.js
@@ -1,10 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
+
 
 function toggleDesktopMenu(){
     console.log('Clic')
@@ -12,5 +16,9 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside(){
+    aside.classList.toggle('inactive');
 }
