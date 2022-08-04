529931401505f27dc3d7fd5ab30c332518432c74
EL Gabo <harcot.sys@gmail.com>
2022-08-03 21:14:49 -0500

    Toogle desktopMenu mobileMenu and Aside

--- yardSale/main.js
+++ yardSale/main.js
@@ -11,14 +11,37 @@ menuCartIcon.addEventListener('click', toggleCartAside);
 
 
 function toggleDesktopMenu(){
-    console.log('Clic')
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    //mobileMenu.classList.toggle('inactive');
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCartAside(){
+    //pregunto si tiene la clase inactive aplicada (antes o despu[es del toggle])
+    //const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    //const isAsideOpen = !aside.classList.contains('inactive');
+    //pregunto si tiene la clase inactive aplicada (antes o despu[es del toggle])
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+     if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
     aside.classList.toggle('inactive');
+
+    
 }
